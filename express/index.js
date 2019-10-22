ce7b1244f7d03231a5179abe039bf082bb99c117
W-Nakasone <psyqhical@icloud.com>
2019-10-22 13:07:09 +0900

    駐車場検索の実装

--- express/index.js
+++ express/index.js
@@ -7,38 +7,50 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 var request = require('request');
-var Coordinates;
-var lat = 35.665662327484;
-var options = {
-    url: 'https://map.yahooapis.jp/search/local/V1/localSearch?output=json&appid=dj00aiZpPWk0clhRdTdrVVd0ZiZzPWNvbnN1bWVyc2VjcmV0Jng9ZDA-&gc=0306005&lat='+ lat +'&lon=139.73091159273&dist=3',
-    method: 'GET',
-    json: true
-}
 
-var array = [];
-var Coordinates;
-request(options, function (error, response, body) {
-
-
-  var Feature = body.Feature;
-  var ResultInfo = body.ResultInfo;
-  var Count = ResultInfo.Count;
-  for ( var i=0;  i<Count;   i++)  {
-
-// 繰り返す処理を書く
-  var Feature_JSON = Feature[i]
-  var Geometry = Feature_JSON.Geometry;
-  Coordinates = Geometry.Coordinates;
-  array[i] = Coordinates;
-  console.log(Coordinates);
-}
-  console.log(array);
-})
 
 
 app.post('/test', function(req, res) {
+  var lat =  req.body.lat
+  var lng =  req.body.lng
+  var options = {
+      url: 'https://map.yahooapis.jp/search/local/V1/localSearch?output=json&appid=dj00aiZpPWk0clhRdTdrVVd0ZiZzPWNvbnN1bWVyc2VjcmV0Jng9ZDA-&gc=0306005&lat='+ lat +'&lon='+ lng +'&dist=3',
+      method: 'GET',
+      json: true
+  }
+  var lat = [];
+  var lng = [];
+  var Feature;
+  var Coordinates;
+  request(options, function (error, response, body) {
+
+
+    Feature = body.Feature;
+
+    var ResultInfo = body.ResultInfo;
+    var Count = ResultInfo.Count;
+    for ( var i=0;  i<Count;   i++)  {
+
+  // 繰り返す処理を書く
+    var Feature_JSON = Feature[i]
+    var Geometry = Feature_JSON.Geometry;
+    Coordinates = Geometry.Coordinates;
+    var result = Coordinates.split(',');
+    lat[i] = result[0];
+    lng[i] = result[1]
+
+  }
+  console.log(lat);
+　console.log(lng);
   res.send({
-    message: array
+    lat: lat,
+    lng: lng,
+
   })
+
+  })
+
+
+
 })
 app.listen(process.env.PORT || 3000)
